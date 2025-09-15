e5c401bd0fe32dd31927251322cf7847075b9b01
pro047 <pro047@naver.com>
2025-09-15 10:54:45 +0900

    deploy

--- src/shared/api/csrfToken.ts
+++ src/shared/api/csrfToken.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+const url = process.env.NEXT_PUBLIC_API_URL;
+
 export const getCsrfToken = async () => {
-  const res = await axios.get('http://localhost:8080/auth/csrf-token');
+  const res = await axios.get(`${url}/auth/csrf-token`);
   return res.data.csrfToken;
 };
